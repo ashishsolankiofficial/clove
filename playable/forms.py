0ab97a209c784b122dc51217890b69b82094b311
ashishsolankiofficial <ashishsolanki.acc@gmail.com>
2023-01-14 11:14:19 +0530

    removed all date time widget

--- playable/forms.py
+++ playable/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from playable.models import Tournament, BilateralMatch
-from util.widgets import BootstrapDateTimePickerInput
 
 
 class DateTimePickerInput(forms.DateTimeInput):
@@ -15,9 +14,7 @@ class TournamentForm(forms.ModelForm):
 
 
 class BilateralMatchForm(forms.ModelForm):
+
     class Meta:
         model = BilateralMatch
         exclude = ['ext_id', 'created_by', 'winner', 'tournament']
-        widgets = {
-            'match_start_time': BootstrapDateTimePickerInput(attrs={'input_format': '%d/%m/%Y %H:%M'}),
-        }
--- playable/views.py
+++ playable/views.py
@@ -52,16 +52,28 @@ def add_match(request, t_id):
             return redirect("list_match", t_id)
     else:
         fm = BilateralMatchForm()
-    return render(request, 'add_match.html', {'form': fm})
+    return render(request, 'add_match.html', {'form': fm, 'tournament_id': t_id})
 
 
 def edit_match(request, t_id, ext_id):
-    return render(request, 'add_match.html')
+    match = get_object_or_404(BilateralMatch, ext_id=ext_id)
+    if request.method == 'POST':
+        fm = BilateralMatchForm(request.POST, instance=match)
+        if fm.is_valid():
+            match = fm.save(commit=False)
+            match.created_by = User.objects.get(ext_id=request.user.ext_id)
+            match.tournament = Tournament.objects.get(ext_id=t_id)
+            match.match_start_time = datetime.strptime(match.match_start_time.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%d-%m %H:%M:%S')
+            match.save()
+            return redirect("list_match", t_id)
+    else:
+        fm = BilateralMatchForm(instance=match)
+    return render(request, 'add_match.html', {'form': fm, 'tournament_id': t_id})
 
 
 def list_match(request, t_id):
     tournament = Tournament.objects.get(ext_id=t_id)
-    matches = BilateralMatch.objects.all().values('ext_id', 'name', 'match_start_time', 'active', 'teamA__name', 'teamB__name', 'winner', 'created_by__display_name')
+    matches = BilateralMatch.objects.filter(tournament__ext_id=t_id).values('ext_id', 'name', 'match_start_time', 'active', 'teamA__name', 'teamB__name', 'winner', 'created_by__display_name')
     match_list = {
         'tournament': tournament,
         'match_list': matches
