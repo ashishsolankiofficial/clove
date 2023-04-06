16b2acef05b860c1281c319b7246dd854c03a032
ashishsolankiofficial <ashishsolanki.acc@gmail.com>
2023-04-06 16:09:57 +0530

    urls updated for dual setup

--- clove/urls.py
+++ clove/urls.py
@@ -8,22 +8,22 @@ from user.serializer import MyTokenObtainPairView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('oj/admin/', admin.site.urls),
 
-    path('portal/', views.login_admin, name='admin_login'),
-    path('portal/admin_logout/', views.logout_admin, name='admin_logout'),
-    path('portal/workflow/', views.show_workflow, name='workflow'),
-    path('portal/user/', include('user.portal_urls')),
-    path('portal/team/', include('team.portal_urls')),
-    path('portal/playable/', include('playable.portal_urls')),
-    path('portal/office/', include('office.portal_urls')),
+    path('oj/portal/', views.login_admin, name='admin_login'),
+    path('oj/portal/admin_logout/', views.logout_admin, name='admin_logout'),
+    path('oj/portal/workflow/', views.show_workflow, name='workflow'),
+    path('oj/portal/user/', include('user.portal_urls')),
+    path('oj/portal/team/', include('team.portal_urls')),
+    path('oj/portal/playable/', include('playable.portal_urls')),
+    path('oj/portal/office/', include('office.portal_urls')),
 
-    path('api/user/', include('user.api_urls')),
-    path('api/playable/', include('playable.api_urls')),
-    path('api/payable/', include('payable.api_urls')),
-    path('api/office/', include('office.api_urls')),
+    path('oj/api/user/', include('user.api_urls')),
+    path('oj/api/playable/', include('playable.api_urls')),
+    path('oj/api/payable/', include('payable.api_urls')),
+    path('oj/api/office/', include('office.api_urls')),
 
-    path('api/auth/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/token-verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('oj/api/auth/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('oj/api/auth/token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('oj/api/auth/token-verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
